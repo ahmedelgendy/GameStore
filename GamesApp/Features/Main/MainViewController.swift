a5cfc3917024a201fe599f34774ecfd0172b99d9
Elgendy <plus.elgendy@gmail.com>
2020-02-10 18:09:06 +0300

    tabBar - change games title, icon and color

--- GamesApp/Features/Main/MainViewController.swift
+++ GamesApp/Features/Main/MainViewController.swift
@@ -11,19 +11,18 @@ import UIKit
 class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tabBar.tintColor = UIColor(named: "tabbar-color")
         self.viewControllers = setupTabBarViewControllers()
     }
     
     func setupTabBarViewControllers() -> [UIViewController] {
-        
         let searchViewController = SearchViewController()
-        searchViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        searchViewController.tabBarItem = UITabBarItem(title: "Games", image: UIImage(named: "games-icon"), tag: 0)
         
         let favoriteViewController = FavoriteViewController()
         favoriteViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
         
-        var viewControllers = [searchViewController, favoriteViewController]
+        let viewControllers = [searchViewController, favoriteViewController]
         return viewControllers.map { UINavigationController(rootViewController: $0) }
     }
 
