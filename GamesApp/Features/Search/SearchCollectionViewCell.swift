0f08fc7fc2835f8976a911b7bbe54ac62596723d
Elgendy <plus.elgendy@gmail.com>
2020-02-11 12:28:36 +0300

    add search cell view model

--- GamesApp/Features/Search/SearchCollectionViewCell.swift
+++ GamesApp/Features/Search/SearchCollectionViewCell.swift
@@ -8,10 +8,48 @@
 
 import UIKit
 
-class SearchCollectionViewCell: UICollectionViewCell {    
+class SearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var metacriticLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func configure(viewModel: SearchCellViewModel) {
+        nameLabel.text = viewModel.name
+        metacriticLabel.text = viewModel.metacritic
+        genreLabel.text = viewModel.genres
+    }
+}
+
+struct SearchCellViewModel {
+    private var game: Game
+    
+    init(game: Game) {
+        self.game = game
+    }
+    
+    var name: String? { game.name }
+    
+    var metacritic: String? {
+        if let metacritic = game.metacritic {
+            return "\(metacritic)"
+        } else {
+            return nil
+        }
+    }
+    
+    var imageURL: URL? {
+        return URL(string: game.backgroundImage ?? "")
+    }
+    
+    var genres: String? {
+        return game.genres.map({ $0.name }).joined(separator: " ,")
+    }
+    
 }
