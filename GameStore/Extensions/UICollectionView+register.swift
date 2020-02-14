8281a9204a5d11b2c435ebaf44735727b70a4e17
Elgendy <plus.elgendy@gmail.com>
2020-02-14 23:11:02 +0300

    rename project

---
+++ GameStore/Extensions/UICollectionView+register.swift
@@ -0,0 +1,39 @@
+//
+//  UICollectionView+register.swift
+//  GameStore
+//
+//  Created by Elgendy on 10.02.2020.
+//  Copyright © 2020 Elgendy. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionView {
+    func register<T: UICollectionViewCell>(cellType: T.Type, bundle: Bundle? = nil) {
+        let className = cellType.className
+        let nib = UINib(nibName: className, bundle: bundle)
+        register(nib, forCellWithReuseIdentifier: className)
+    }
+
+    func register<T: UICollectionViewCell>(cellTypes: [T.Type], bundle: Bundle? = nil) {
+        cellTypes.forEach { register(cellType: $0, bundle: bundle) }
+    }
+
+    func register<T: UICollectionReusableView>(reusableViewType: T.Type, ofKind kind: String = UICollectionView.elementKindSectionHeader,  bundle: Bundle? = nil) {
+        let className = reusableViewType.className
+        let nib = UINib(nibName: className, bundle: bundle)
+        register(nib, forSupplementaryViewOfKind: kind, withReuseIdentifier: className)
+    }
+
+    func register<T: UICollectionReusableView>(reusableViewTypes: [T.Type], ofKind kind: String = UICollectionView.elementKindSectionHeader, bundle: Bundle? = nil) {
+        reusableViewTypes.forEach { register(reusableViewType: $0, ofKind: kind, bundle: bundle) }
+    }
+
+    func dequeueReusableCell<T: UICollectionViewCell>(with type: T.Type, for indexPath: IndexPath) -> T {
+        return dequeueReusableCell(withReuseIdentifier: type.className, for: indexPath) as! T
+    }
+
+    public func dequeueReusableView<T: UICollectionReusableView>(with type: T.Type, for indexPath: IndexPath, ofKind kind: String = UICollectionView.elementKindSectionHeader) -> T {
+        return dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: type.className, for: indexPath) as! T
+    }
+}
---
+++ GameStore/Shared/Repositories/FavoriteRepository.swift
@@ -0,0 +1,69 @@
+//
+//  FavoriteRepository.swift
+//  GameStore
+//
+//  Created by Elgendy on 11.02.2020.
+//  Copyright © 2020 Elgendy. All rights reserved.
+//
+
+import Foundation
+
+protocol FavoriteRepositoryProtocol {
+    var gamesIds: [String] { get }
+    func addGame(_ game: GameDetails)
+    func removeItem(_ game: GameDetails)
+    func getItems() -> [GameDetails]
+    func isItemFavorited(id: String) -> Bool
+}
+
+final class FavoriteRepository: FavoriteRepositoryProtocol {
+    
+    let favoritedKey = "favoritedItems"
+    
+    var gamesIds: [String] {
+        get {
+            LocalStorage.getValue(for: favoritedKey) as? [String] ?? []
+        } set {
+            LocalStorage.add(value: newValue, forKey: favoritedKey)
+        }
+    }
+    
+    func getItems() -> [GameDetails] {
+        var games = [GameDetails]()
+        gamesIds.forEach { (gameId) in
+            if let gameJson = LocalStorage.getValue(for: gameId) as? Data {
+                do {
+                    let gameObject = try JSONDecoder().decode(GameDetails.self, from: gameJson)
+                    games.append(gameObject)
+                } catch { }
+            }
+        }
+        return games
+    }
+    
+    func addGame(_ game: GameDetails) {
+        do {
+            let gameJson = try JSONEncoder().encode(game)
+            LocalStorage.add(value: gameJson, forKey: game.storageId)
+        } catch { }
+        if !gamesIds.contains(game.storageId) {
+            gamesIds.append(game.storageId)
+        }
+        fireObserver()
+    }
+    
+    func removeItem(_ game: GameDetails) {
+        gamesIds = gamesIds.filter { $0 != game.storageId }
+        LocalStorage.remove(key: game.storageId)
+        fireObserver()
+    }
+    
+    func isItemFavorited(id: String) -> Bool {
+        return gamesIds.contains(id)
+    }
+    
+    private func fireObserver() {
+        NotificationCenter.default.post(name: .favoritedItemsUpdated,
+                                        object: nil)
+    }
+}
